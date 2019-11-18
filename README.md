f4cb1e055c57ed37ad65d0ddbf78d9b22df3d486
coderick <dev.erick95@gmail.com>
2019-11-18 00:56:26 -0500

    Github pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# V 1.0.0
-Mi portafolio personal.
--- dev/js/app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger'),
-	nav = document.getElementById('nav')
-
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('is-active')
-	nav.classList.toggle('nav-active')
-})
